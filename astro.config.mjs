e048ad32f4b7d43a90a41a17002e1e0e8532b7c4
HoppaySama <marclesterchernandez@gmail.com>
2026-08-09 20:11:31 +0800

    Remove GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://hoppay-sama.github.io',
-  base: '/Maghapon-Cafe',
-});
+export default defineConfig({});
